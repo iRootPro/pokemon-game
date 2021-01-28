70ecdb2da1a14c50f64668008723357f407b9c1e
Alexander Neupokoev <admin@armavir.ru>
2021-01-28 08:30:04 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import bg3 from './assets/bg3.jpg'
 function App() {
     return (
         <div className="App">
-            <Header title={'This is title'} descr={'This is Description!'} />
-            <Layout title={'Заголовок для Layout 1'} descr={'Описание для layout 1'} id={1} urlBg={bg1} />
-            <Layout title={'Заголовок для Layout 2'} descr={'Описание для layout 2'} id={2} colorBg='green'/>
-            <Layout title={'Заголовок для Layout 3'} descr={'Описание для layout 3'} id={3} urlBg={bg3}/>
+            <Header title='This is title' descr='This is Description!' />
+            <Layout title='Заголовок для Layout 1' descr='Описание для layout 1' id={1} urlBg={bg1} />
+            <Layout title='Заголовок для Layout 2' descr='Описание для layout 2' id={2} colorBg='green'/>
+            <Layout title='Заголовок для Layout 3' descr='Описание для layout 3' id={3} urlBg={bg3}/>
             <Footer />
         </div>
     );
