66c7be86d7a53758ffb80e3eda1eb956055651fc
Alexander Neupokoev <admin@armavir.ru>
2021-02-03 11:21:16 +0300

    refactor menu

--- src/components/MenuHeader/Menu/index.js
+++ src/components/MenuHeader/Menu/index.js
@@ -3,31 +3,18 @@ import styles from './style.module.css'
 import cn from 'classnames'
 
 const Menu = ({isOpen}) => {
+    const links = [
+        {name: 'HOME', href: "#welcome"},
+        {name: 'GAME', href: "#game"},
+        {name: 'ABOUT', href: "#about"},
+        {name: 'CONTACT', href: "#contact"}
+    ]
     return (
         <div className={cn(styles.menuContainer, {[styles.active]: isOpen})}>
             <div className={styles.overlay}/>
             <div className={styles.menuItems}>
                 <ul>
-                    <li>
-                        <a href="#welcome">
-                            HOME
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#game">
-                            GAME
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#about">
-                            ABOUT
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#contact">
-                            CONTACT
-                        </a>
-                    </li>
+                    {links.map(link => <li><a href={link.href}>{link.name}</a></li>)}
                 </ul>
             </div>
         </div>
